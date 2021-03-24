739f530480776506c043c02e6ce60c5866152568
Dashkata <alexandur04@abv.bg>
2021-03-24 14:18:50 +0200

    Added the database!

--- WildLifeSpotter2/WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/MainActivity.java
+++ WildLifeSpotter2/WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/MainActivity.java
@@ -2,20 +2,53 @@ package com.example.wildlifespotter;
 
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
     public void launchPinLocation(View v){
         Intent i = new Intent(this, PinLocationMenu.class);
@@ -25,5 +58,26 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, BrowseMapMenu.class);
         startActivity(i);
     }
+    public void printData(View v){
+        TextView t = findViewById(R.id.Test);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 }
+
+
+
+
--- WildLifeSpotter2/WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/PlantInfoActivity.java
+++ WildLifeSpotter2/WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/PlantInfoActivity.java
@@ -13,12 +13,7 @@ public class PlantInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plant_info);
-        TextView plant_name = (TextView)findViewById(R.id.plant_Name);
-        String plant_name_value = getIntent().getExtras().getString("plant_name_value");
-        plant_name.setText(plant_name_value);
-        TextView plant_info = (TextView)findViewById(R.id.plant_info);
-        String plant_info_value = getIntent().getExtras().getString("plant_info_value");
-        plant_info.setText(plant_info_value);
+
     }
     public void goBackToPinMenu(View v){
         Intent i = new Intent(this, PinLocationMenu.class);
