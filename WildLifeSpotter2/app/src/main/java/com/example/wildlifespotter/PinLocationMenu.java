39a687113b9b744998ef2d0b5fea9b02239e11db
Dashkata <alexandur04@abv.bg>
2021-04-10 02:13:59 +0300

    nz

--- WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/PinLocationMenu.java
+++ WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/PinLocationMenu.java
@@ -2,33 +2,19 @@ package com.example.wildlifespotter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 
 public  class PinLocationMenu extends AppCompatActivity {
-    Spinner spinner;
-    Button button;
 
     @SuppressLint("ResourceType")
     @Override
