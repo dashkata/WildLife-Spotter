f15b39be0e26ee1c22162d16e92037a29d9c372b
1freshie <hristian.todorov14@gmail.com>
2021-04-10 15:31:28 +0300

    Локацията е готова

--- WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/MainActivity.java
+++ WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.wildlifespotter;
 
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
