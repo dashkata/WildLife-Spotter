bec0d7728b2ec773b5b6e19563086d985ea643b2
bobomaika <georgistan@abv.bg>
2021-04-14 18:06:44 +0300

    на ти browse map

--- WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/BrowseMapMenu.java
+++ WildLifeSpotter2/app/src/main/java/com/example/wildlifespotter/BrowseMapMenu.java
@@ -13,13 +13,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firestore.v1.StructuredQuery;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -104,7 +110,11 @@ public class BrowseMapMenu extends AppCompatActivity{
 //        Intent i = new Intent(this, MainActivity.class);
 //        startActivity(i);
 //    }
+TextView tekst;
 FirebaseFirestore db = FirebaseFirestore.getInstance();
+CollectionReference Regions = db.collection("Regions");
+//private Query RegionsRef;
+//Query query = RegionsRef.whereEqualTo("Animal_Info", "");
 private Spinner spinner;
 
 
@@ -114,6 +124,7 @@ private Spinner spinner;
         setContentView(R.layout.activity_browse_map_menu);
 
         spinner = findViewById(R.id.spinner2);
+        tekst = findViewById(R.id.textView);
 
         List<String> categories = new ArrayList<>();
         categories.add(0, "Изберете регион");
@@ -139,21 +150,59 @@ private Spinner spinner;
                 }
                 else
                 {
-                    db.collection("Animals Dunavska ravnina")
-                            .whereEqualTo("Балканска кротушка", true)
-                            .get()
-                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                    if (task.isSuccessful()) {
-                                        for (QueryDocumentSnapshot document : task.getResult()) {
-                                            Log.d("", document.getId() + " => " + document.getData());
-                                        }
-                                    } else {
-                                        Log.d("", "Error getting documents: ", task.getException());
-                                    }
+//                    db.collection("Regions")
+//                            .get()
+//                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                    if (task.isSuccessful()) {
+//                                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                                            Log.d("Showing data", document.getId() + " => " + document.getData());
+//                                        }
+//                                    } else {
+//                                        Log.d("", "Error getting documents: ", task.getException());
+//                                    }
+//                                }
+//                            });
+
+                    DocumentReference docRef = db.collection("Regions").document("Region_Dunavska_ravnina");
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    Log.d("слави трифонов", "DocumentSnapshot data: " + document.getData());
+
+                                    tekst.setText("" + document.getData());
+
+                                } else {
+                                    Log.d("слави трифонов", "No such document");
                                 }
-                            });
+                            } else {
+                                Log.d("слави трифонов", "get failed with ", task.getException());
+                            }
+                        }
+                    });
+
+
+
+
+//                    DocumentReference docRef = FirebaseFirestore.getInstance().collection("Regions").document("Region_Dunavska_ravnina").collection("Animals Dunavska ravnina").document("Балканска кротушка");
+//                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                            if(task.isSuccessful()){
+//                                DocumentSnapshot doc = task.getResult();
+//                                if(doc.exists()){
+//                                    Log.d("Document", doc.getData().toString());
+//                                } else {
+//                                    Log.d("Document", "No data");
+//                                }
+//                            }
+//                        }
+//                    });
+
                     //on selecting a spinner item
                     String item = parent.getItemAtPosition(position).toString();
 
@@ -186,6 +235,7 @@ private Spinner spinner;
                     }
                 }
             }
+
 
 
 
